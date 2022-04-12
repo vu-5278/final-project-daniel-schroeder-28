d43f20bacfe477cb0107995916e7d86484b9c5ff
Daniel Schroeder <schroederdan98@yahoo.com>
2022-04-11 22:30:03 -0400

    initial project structure

---
+++ app/src/main/java/com/vanderbilt/flashcardapp/AddSetActivity.java
@@ -0,0 +1,14 @@
+package com.vanderbilt.flashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddSetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_set);
+    }
+}
---
+++ app/src/main/java/com/vanderbilt/flashcardapp/LoginScreenActivity.java
@@ -0,0 +1,14 @@
+package com.vanderbilt.flashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
